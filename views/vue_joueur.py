bd5e805528f54af9efdbfa1c58ec981d8ab21fe8
Steve <claudefrancois@gmail.com>
2025-04-25 18:47:57 +0200

    Ajout de la vérification de la date de naissance au format date grace à strptime()

--- views/vue_joueur.py
+++ views/vue_joueur.py
@@ -1,5 +1,5 @@
 import re
-
+from datetime import datetime
 
 def valider_identifiant_national(identifiant):
     """Valide l'identifiant national selon le format 'ID12345'"""
@@ -7,24 +7,28 @@ def valider_identifiant_national(identifiant):
     if re.match(pattern, identifiant):
         return True
     else:
-        print(
-            "Erreur : L'identifiant national doit être au format 'ID12345' (deux lettres majuscules + cinq chiffres).")
+        print("Erreur : L'identifiant national doit être au format 'ID12345' (deux lettres majuscules + cinq chiffres).")
         return False
 
+def valider_date_naissance(date_naissance):
+    """Valide la date de naissance au format 'YYYY-MM-DD'"""
+    try:
+        datetime.strptime(date_naissance, '%Y-%m-%d')
+        return True
+    except ValueError:
+        print("Erreur : La date de naissance doit être au format 'YYYY-MM-DD'.")
+        return False
 
 def afficher_joueur(joueur):
     """Affiche les informations d'un joueur"""
-    print(
-        f"{joueur.nom} {joueur.prenom} (Identifiant National: {joueur.identifiant_national}, "
-        f"Classement: {joueur.classement}, Né(e) le: {joueur.date_naissance})")
-
+    print(f"{joueur.nom} {joueur.prenom} (Identifiant National: {joueur.identifiant_national}, "
+          f"Classement: {joueur.classement}, Né(e) le: {joueur.date_naissance})")
 
 def afficher_liste_joueurs(joueurs):
     """Affiche la liste de tous les joueurs"""
     print("\nListe des joueurs :")
     for i, joueur in enumerate(joueurs, 1):
         afficher_joueur(joueur)
-
 
 def obtenir_donnees_joueur():
     """Demande à l'utilisateur de saisir les données d'un joueur avec validation"""
@@ -39,6 +43,11 @@ def obtenir_donnees_joueur():
             break  # Sortie de la boucle si l'identifiant est valide
 
     classement = input("Classement : ")
-    date_naissance = input("Date de naissance (YYYY-MM-DD) : ")
+
+    # Validation de la date de naissance
+    while True:
+        date_naissance = input("Date de naissance (YYYY-MM-DD) : ")
+        if valider_date_naissance(date_naissance):
+            break  # Sortie de la boucle si la date est valide
 
     return nom, prenom, identifiant, classement, date_naissance
